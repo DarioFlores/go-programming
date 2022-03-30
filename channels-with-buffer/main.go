2fb625a1a6b8ff0f7a26933f247ee749703b3f96
DarioFlores <dario.dflores@mercadolibre.com>
2022-03-30 18:42:08 -0300

    channels with and without buffer

--- channels-with-buffer/main.go
+++ channels-with-buffer/main.go
@@ -5,13 +5,9 @@ import (
 )
 
 func main() {
-	c := make(chan int)
+	c := make(chan int, 1)
 
-	// c <- 49 // fatal error: all goroutines are asleep - deadlock!
-	go func() {
-		// time.Sleep(10 * time.Second) // Si lo pongo el Println espera hasta que llegue un resultado
-		c <- 49
-	}()
+	c <- 49
 
 	fmt.Println(<-c)
 }
--- channels-without-buffer/main.go
+++ channels-without-buffer/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	c := make(chan int)
+
+	// c <- 49 // fatal error: all goroutines are asleep - deadlock!
+	go func() {
+		// time.Sleep(10 * time.Second) // Si lo pongo el Println espera hasta que llegue un resultado
+		c <- 49
+	}()
+
+	fmt.Println(<-c)
 }
